2d1db297afcd48dfe9a88bf20ac3e2d492d5d55c
jamesperalta <jamesperalta35@gmail.com>
2020-02-15 01:04:53 -0700

    Have the race track and race car moving

---
+++ Simulation/Track-Simulation/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3( target.position.x, target.position.y, -10f);
+    }
+}
---
+++ Simulation/Track-Simulation/Assets/Scripts/Car2DController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car2DController : MonoBehaviour
+{
+    float speedForce = 10f;
+    float torqueForce = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.up * speedForce);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(1 * torqueForce);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(-1 * torqueForce);
+        }
+    }
+}
